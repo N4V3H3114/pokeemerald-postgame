c671bd5d8f81c157715f07ae17cc6d1d7e8a4192
Nave Nella <evanallen98@comcast.net>
2023-07-21 07:31:10 -0500

    Building out connecting events

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -42,9 +42,9 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 
 #define FLAG_NEO_AQUA_LILYCOVE_MET    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
+#define FLAG_ROUTE121_SHOW_RIVAL    0x21 // Unused Flag
+#define FLAG_FABLED_ISLAND_FINISH    0x22 // Unused Flag
+#define FLAG_FABLED_ISLAND_START    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
